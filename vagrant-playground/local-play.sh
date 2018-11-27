b86ea0bbd2745714f0b7a0171c972868d35bcf9a
Martin Grunbaum <martin@itsolveonline.net>
2018-11-27 13:51:31 +0100

    Update local-play.sh and ansible.cfg.example relative paths

--- vagrant-playground/local-play.sh
+++ vagrant-playground/local-play.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant -i ../hosts/hosts.local $@
+ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant $@
